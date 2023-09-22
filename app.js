122a887e29bd8f0e61b29cb5624a1df93b9def15
tak <wontaek22@gmail.com>
2023-09-22 20:19:51 +0900

    23.09.22 로그인 작업중 + localStorage 기능 추가

--- app.js
+++ app.js
@@ -2,21 +2,38 @@ const loginForm     = document.querySelector("#login-form");
 const loginInput    = document.querySelector("#login-form input");
 const greeting      = document.querySelector("#greeting");
 
-const HIDDEN_CLASSNAME = "hidden";
+const HIDDEN_CLASSNAME  = "hidden";
+const USERNAME_KEY      = "username";
 
+/**
+ * Log In 버튼 클릭 시 이벤트
+ */
 function onLoginSubmit(event) {
-    event.preventDefault();
+    event.preventDefault();                         //브라우저의 기본 동작 막음 이후부터는 원하는 대로 조작 가능
     loginForm.classList.add(HIDDEN_CLASSNAME);
     const username = loginInput.value;
-    //greeting.innerText = "Hello " + username;
-    greeting.innerText = `Hello ${username}`;   // '', "" 는 작동하지않는다 `만 작동한다
-    greeting.classList.remove(HIDDEN_CLASSNAME);
-    console.log("그냥 커밋테스트 코드 추가");
+    localStorage.setItem(USERNAME_KEY, username);     //입력한 이름을 localStorage를 통해 일시적으로 불러올 수 있음
+    paintGreeting(username);
 }
- 
+
+/**
+ * show name 공통함수
+ */
+function paintGreeting (username) {
+    greeting.innerText = `Hello ${username}`;       // '', "" 는 작동하지않는다 `만 작동한다 -> greeting.innerText = "Hello " + username;
+    greeting.classList.remove(HIDDEN_CLASSNAME);
+    
+}
+
+const savedUsername = localStorage.getItem(USERNAME_KEY);
+
+if (savedUsername === null) {   //show the form
+    loginForm.classList.remove(HIDDEN_CLASSNAME);
+    loginForm.addEventListener("submit", onLoginSubmit);
+} else {                        //show the greeting
+    paintGreeting(savedUsername);
+}
 
 
-loginForm.addEventListener("submit", onLoginSubmit);
- 
 
 
