d8a19b80e542f1a18832e4443165dd77716fc3ad
tak <wontaek22@gmail.com>
2023-10-01 15:56:24 +0900

    23.10.01 To-Do List 기능 구현중 (localStorage 저장)

--- js/todo.js
+++ js/todo.js
@@ -2,14 +2,38 @@ const toDoForm = document.getElementById("todo-form");
 const toDoInput = document.querySelector("#todo-form input");
 const toDoList = document.getElementById("todo-list");
 
+const TODOS_KEY = "todos";
+
+const toDos = [];
+
+/**
+ * toDoList 저장 (작성 시 localStorage에 저장)
+ */
+function saveToDos () {
+    localStorage.setItem("todos", JSON.stringify(toDos));
+
+}
+
+/**
+ * toDoList 삭제 ("X" button 클릭 시)
+ */
+function deleteToDo (event) {
+    const li = event.target.parentElement;
+    li.remove();
+}
+
 /**
  * 입력한 ToDo 추가 함수
  */
 function paintToDo (newTodo) {
     const li = document.createElement("li");
     const span = document.createElement("span");
-    li.appendChild(span);
     span.innerText = newTodo;
+    const button = document.createElement("button");
+    button.innerText = "X";
+    button.addEventListener("click", deleteToDo);
+    li.appendChild(span);
+    li.appendChild(button);
     toDoList.appendChild(li);
 }
 
@@ -20,7 +44,20 @@ function handleToDoSubmit (event) {
     event.preventDefault();
     const newTodo = toDoInput.value;
     toDoInput.value = "";
-    paintToDo(newTodo);
+    toDos.push(newTodo);
+    paintToDo(newTodo);     //목록에 보여지도록 추가
+    saveToDos();            //localStorage에 추가
 }
 
 toDoForm.addEventListener("submit", handleToDoSubmit);
+
+const savedToDos = localStorage.getItem(TODOS_KEY);
+console.log(savedToDos);
+
+if (savedToDos !== null) {
+    const parsedToDos = JSON.parse(savedToDos);
+    parsedToDos.forEach((item) => console.log("this is turn of ", item));
+}
+
+
+
